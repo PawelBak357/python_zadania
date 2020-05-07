d4232b5eb3d7c418cd363c4c1d1f2a25f1bffcc8
Pawel Bak <pawel.aleksander.bak@gmail.com>
2020-05-07 20:40:13 +0200

    Zadanie 3_2

---
+++ zad_3_2.py
@@ -0,0 +1,32 @@
+def liczba_dni_miesiaca(a: str) -> int:
+    """
+    Podaje liczbę dni dla podanej nazwy miesiąca
+    :param a: nazwa miesiąca
+    :return: liczba dni
+    """
+    x_31 = ['styczen', 'marzec', 'maj', 'lipiec', 'sierpien', 'pazdziernik', 'grudzien']
+    x_30 = ['kwiecien', 'czerwiec', 'wrzesien', 'listopad']
+    x_l = ['luty']
+    if a in x_31:
+        return 31
+    elif a in x_30:
+        return 30
+    elif a in x_l:
+        return liczba_dni_lutego(int(input("Podaj rok: ")))
+    else:
+        raise ValueError("Zła nazwa!")
+
+def liczba_dni_lutego(a: int) -> int:
+    """
+    Sprawdza, czy rok jest przestępny czy nie i zwraca liczbę dni lutego
+    :param a: rok
+    :return: liczba dni lutego
+    """
+    if a % 4 == 0 and a % 400 != 0:
+        return 29
+    else:
+        return 28
+
+miesiac = input("Podaj nazwę miesiąca: ")
+print(liczba_dni_miesiaca(miesiac))
+
